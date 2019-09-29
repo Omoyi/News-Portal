7e1ebf85881c9defca13899108a03af8a15e6de0
Omoyi <urakhel7@gmail.com>
2019-09-29 21:31:31 +0200

    create a file

--- src/main/java/models/NewsInfo.java
+++ src/main/java/models/NewsInfo.java
@@ -1,4 +1,38 @@
 package models;
+import java.util.Objects;
 
 public class NewsInfo {
+    private String header;
+    private String content;
+    private int id;
+
+    public NewsInfo(String header, String content){
+        this.content = content;
+        this.header = header;
+    }
+
+    public String getHeader(){
+        return header;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
