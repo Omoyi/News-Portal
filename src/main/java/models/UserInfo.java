38d62cbc9a7795f283f3b2553a4150b63f91f199
Omoyi <urakhel7@gmail.com>
2019-09-29 21:42:02 +0200

    create another file in package model

--- src/main/java/models/UserInfo.java
+++ src/main/java/models/UserInfo.java
@@ -1,4 +1,38 @@
 package models;
 
 public class UserInfo {
+    private String post;
+    private String role;
+    private int id;
+
+    public UserInfo(String post, String role){
+        this.role = role;
+        this.post = post;
+    }
+
+    public String getPost(){
+        return post;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setContent(String content) {
+        this.post = post;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
+
