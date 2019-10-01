63c98565d1ee1c95f39140e3d22d8e277f3aa8c4
Omoyi <urakhel7@gmail.com>
2019-10-01 21:52:35 +0200

    declare and set variables

--- src/main/java/models/NewsInfo.java
+++ src/main/java/models/NewsInfo.java
@@ -1,5 +1,8 @@
 package models;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class NewsInfo {
 
     private String header;
@@ -7,31 +10,42 @@ public class NewsInfo {
     private int deptInfId;
     private String author;
     private int id;
+    private long createdAt;
+    private String formatCreatedAt;
 
-    public NewsInfo(String header, String content, String author){
+
+    public NewsInfo(String header, String content, String author, long createdAt, String formatCreatedAt){
         this.content = content;
         this.header = header;
         this.author = author;
+        this.createdAt = System.currentTimeMillis();;
+        setFormatCreatedAt();
         this.deptInfId = deptInfId;
     }
 
     public String getHeader(){ return header; }
-    public String getContent(){
-        return content;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setHeader(String header) {
-        this.header = header;
-    }
+    public String getContent(){ return content; }
+    public int getId() { return id; }
+    public void setHeader(String header) { this.header = header; }
     public void setContent(String content) {  this.content = content; }
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
     public String getAuthor() { return author; }
     public void setAuthor(String author) { this.author = author; }
     public int getDeptInfId() { return deptInfId; }
     public void setDeptInfId(int deptInfId) { this.deptInfId = deptInfId; }
+    public long getCreatedAt() { return createdAt; }
 
+    public String getFormatCreatedAt(){
+        Date dates = new Date(createdAt);
+        String PatternToUse = "MM/dd/yyyy @ K:mm a";
+        SimpleDateFormat sdf = new SimpleDateFormat(PatternToUse);
+        return sdf.format(dates);
+    }
+
+    public void setFormatCreatedAt(){
+        Date dates = new Date(this.createdAt);
+        String PatternToUse = "MM/dd/yyyy @ K:mm a";
+        SimpleDateFormat sdf = new SimpleDateFormat(PatternToUse);
+        this.formatCreatedAt = sdf.format(dates);
+    }
 }
