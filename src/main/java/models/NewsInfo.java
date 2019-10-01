1e45fe336926a6eef3a670c566b6ac44112a13f7
Omoyi <urakhel7@gmail.com>
2019-10-01 21:14:57 +0200

    create getters and setters

--- src/main/java/models/NewsInfo.java
+++ src/main/java/models/NewsInfo.java
@@ -1,39 +1,37 @@
 package models;
-import java.util.Objects;
 
 public class NewsInfo {
 
     private String header;
     private String content;
     private int deptInfId;
-    private author
+    private String author;
     private int id;
 
-
-    public NewsInfo(String header, String content){
+    public NewsInfo(String header, String content, String author){
         this.content = content;
         this.header = header;
+        this.author = author;
         this.deptInfId = deptInfId;
     }
 
     public String getHeader(){ return header; }
-
     public String getContent(){
         return content;
     }
-
     public int getId() {
         return id;
     }
-
     public void setHeader(String header) {
         this.header = header;
     }
-
     public void setContent(String content) {  this.content = content; }
-
     public void setId(int id) {
         this.id = id;
     }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public int getDeptInfId() { return deptInfId; }
+    public void setDeptInfId(int deptInfId) { this.deptInfId = deptInfId; }
 
 }
